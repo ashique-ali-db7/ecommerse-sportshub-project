76a925f69eb8dd5cc63121d9f1898da6beca717a
ashique ali <ashiquealikmvkd@gmial.com>
2021-12-02 19:20:55 +0530

    cart responsive design

--- routes/users.js
+++ routes/users.js
@@ -155,7 +155,7 @@ router.get('/shopping-cart',verifyLoginForLoginpage, async(req, res, next) =>{
   }else{
     cartItemsEmpty = true;
   }
-  res.render('users/shopping-cart',{ admin:false,user,notheader:true,cartItems,total,cartItemsEmpty});
+  res.render('users/shoppingnewcart',{ admin:false,user,notheader:true,cartItems,total,cartItemsEmpty});
 
 });
 
